e21759e5f16c929a1a100070231132bdf8c81812
nikolaiandrov <nikolaiandrov@abv.bg>
2024-08-29 21:21:33 +0300

    initial create

---
+++ FileBox/FileBox/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FileBox.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
