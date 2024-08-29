a098cef44f05fac75ad7b8683ff2514522958a6d
nikolaiandrov <nikolaiandrov@abv.bg>
2024-08-29 23:03:59 +0300

    button and controller for file added

---
+++ FileBox/FileBox/Controllers/FileController.cs
@@ -0,0 +1,13 @@
+﻿namespace FileBox.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class FileController : Controller
+    {
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+    }
+}
